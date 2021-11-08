e54da5055e443218fb615ac7d2c6b2247e3a3921
Sunny Bedi <sunnybedi002@yahoo.com>
2021-11-08 08:37:24 -0800

    4 task completed

--- index.js
+++ index.js
@@ -28,11 +28,13 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+   - counter 1 has correct closure and will store the ammount of times it is called
   2. Which of the two uses a closure? How can you tell?
-  
+    -number 1 because it has the variable inside of the function not outside
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+      -  counter one would be to keep track of how many times the counter was called 
+     counter two would be to simply add slowly
 */
 
 // counter1 code
@@ -62,8 +64,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random() * Math.floor(3))
+
 }
 
 
@@ -80,9 +83,16 @@ Use the finalScore function below to do the following:
   "Away": 5
 }
 */ 
+function finalScore(inningCB, numberOfInnings){
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+  const score = { 'Home': 0 , 'Away': 0}
+
+  for (let i = 0; i < numberOfInnings; i++) {
+    score.Away += inningCB();
+    score.Home += inningCB();
+  }
+   return score;
+
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +100,8 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {'Away': inningCB(),'Home':inningCB()};
 }
 
 
